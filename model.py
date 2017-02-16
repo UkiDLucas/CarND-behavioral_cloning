adbc632af5328aca4656f25a51be425a2699e31a
Uki D. Lucas <UkiDLucas@gmail.com>
2017-02-16 15:20:53 -0600

    testing max CNN params the GPU can run

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ processed_images_dir = "processed_images/"
 
 model_dir = "../_DATA/MODELS/"
 model_name = "model_p3_14x64x3_"
-batch_size = 64
+batch_size = 256
 nb_epoch = 3 
 # 30 epochs = 55 minutes on MacBook Pro
 
@@ -45,7 +45,7 @@ import DataHelper
 # https://github.com/aymericdamien/TensorFlow-Examples/issues/38#issuecomment-265599695
 import tensorflow as tf
 
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5) #0.333
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8) #0.333
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True, gpu_options=gpu_options))
 
 
@@ -274,7 +274,7 @@ model = Sequential()
 model.add(Convolution2D(32, 3, 3, border_mode='same', activation="relu" ,
                         input_shape=(14, 64 ,3), dim_ordering='tf', name="conv2d_1_relu"))
 model.add(Convolution2D(32, 3, 3, border_mode='same', activation="relu", name="conv2d_2_relu" ))
-model.add(Convolution2D(32, 5, 5, border_mode='same', activation="relu", name="conv2d_3_relu" ))
+model.add(Convolution2D(64, 5, 5, border_mode='same', activation="relu", name="conv2d_3_relu" ))
 
 model.add(Flatten())
 
@@ -334,31 +334,15 @@ history = model.fit(training_features, y_one_hot, nb_epoch=nb_epoch,
 # # Previous results
 CPU:
 Train on 7971 samples, validate on 1993 samples
-Epoch 1/3
-7971/7971 [==============================] - 60s - loss: 0.1146 - acc: 0.1283 - val_loss: 0.0235 - val_acc: 0.2820 
-Epoch 2/3
-7971/7971 [==============================] - 56s - loss: 0.0238 - acc: 0.2855 - val_loss: 0.0216 - val_acc: 0.3437 
-Epoch 3/3
-7971/7971 [==============================] - 55s - loss: 0.0221 - acc: 0.3419 - val_loss: 0.0207 - val_acc: 0.3532 
+55s - loss: 0.0221 - acc: 0.3419 - val_loss: 0.0207 - val_acc: 0.35 
 
 GPU:
 Train on 7971 samples, validate on 1993 samples
-Epoch 1/3
-7971/7971 [==============================] - 24s - loss: 0.2050 - acc: 0.1242 - val_loss: 0.0230 - val_acc: 0.2614 
-Epoch 2/3
-7971/7971 [==============================] - 17s - loss: 0.0228 - acc: 0.3002 - val_loss: 0.0215 - val_acc: 0.3482 
-Epoch 3/3
-7971/7971 [==============================] - 17s - loss: 0.0215 - acc: 0.3611 - val_loss: 0.0206 - val_acc: 0.3643 
-
+17s - loss: 0.0215 - acc: 0.3611 - val_loss: 0.0206 - val_acc: 0.36 
 
 CPU:
 Train on 2501 samples, validate on 626 samples
-Epoch 1/5
-2501/2501 [==============================] - 109s - loss: 0.0207 - acc: 0.3135 - val_loss: 0.0199 - val_acc: 0.3642
-Epoch 2/5
-2501/2501 [==============================] - 106s - loss: 0.0207 - acc: 0.3083 - val_loss: 0.0199 - val_acc: 0.3594
-Epoch 3/5
-2501/2501 [==============================] - 106s - loss: 0.0207 - acc: 0.3071 - val_loss: 0.0199 - val_acc: 0.3594
+21s - loss: 0.0244 - acc: 0.3000 - val_loss: 0.0219 - val_acc: 0.37
 
 __
 Layer (type)                     Output Shape          Param #     Connected to                     
